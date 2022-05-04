5608c7b0a71abbced52a5aa3cda089ebb0f00a0b
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-05-04 11:04:16 -0300

    criação das classes de modelo da aplicação

---
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -0,0 +1,24 @@
+package com.dio.santander.bankline.api.model;
+
+public class Conta {
+	//Variables Declarations
+	private Long numero;
+	private Double saldo;
+	
+	//Getters and Setters
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
